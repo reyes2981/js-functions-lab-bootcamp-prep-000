877df01b05c80914b97d3566481e19e49dd930fb
diplomatic-link-4183 <noe.reyes92@yahoo.com>
2020-09-20 03:39:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ function happyCustomHolidayTo(Christmas, Larry) {
   console.log(`Happy ${holiday}, ${name}!`)
 }
 
-function holidayCountdown(Halloween, 45) {
+function holidayCountdown(Halloween, "45") {
   console.log(`It's ${days} days until ${holiday}!`)
 }
