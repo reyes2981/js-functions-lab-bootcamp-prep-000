3b87a455049f9ebb37f11de24139df8a206d9f16
diplomatic-link-4183 <noe.reyes92@yahoo.com>
2020-09-20 03:47:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 // write your code below!
 function happyHolidays() {
-  console.log("Happy holidays!")
+  return "Happy holidays!"
 }
 
-function happyHolidaysTo(Larry) {
-  console.log(`Happy holidays, ${name}!`)
+function happyHolidaysTo(name){
+  return `Happy holidays, ${name}!`
 }
 
-function happyCustomHolidayTo(Christmas, Larry) {
-  console.log(`Happy ${holiday}, ${name}!`)
+function happyHolidayTo(holiday, name){
+  return `Happy ${holiday}, ${name}!`
 }
 
 function holidayCountdown(holiday, days){
-  return `It's ${days} days until ${holiday}!`
+  return `It\'s ${days} days until ${holiday}!`
 }
